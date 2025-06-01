f96c5b162a81aca3063bf0d2f1912a9c536c4c0f
Vijay M <vijaymurugan177@gmail.com>
2025-06-01 22:19:13 +0530

    CLEAN UP

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 const Hero = () => {
   return (
     <section className="relative hero-section flex flex-col items-center justify-center min-h-screen px-4 overflow-hidden">
+      {/* Background circles */}
       <div className="hero-gradient absolute w-[1000px] h-[1000px] -z-10">
         <Image
           src="/images/circles.png"
@@ -15,25 +16,30 @@ const Hero = () => {
         />
       </div>
 
+      {/* Hero content */}
       <div className="hero-content mt-40 mb-12 flex flex-col items-center justify-center text-center max-w-4xl mx-auto">
         <Badge />
 
         <h1 className="hero-title font-medium leading-[48px] md:leading-[82px] text-[42px] md:text-[82px] hero-text-animate hero-gradient-text mb-6">
           Boost your <br /> rankings with AI.
         </h1>
+        {/* Mobile text */}
         <p className="block md:hidden text-lg text-white mb-6 max-w-2xl hero-text-animate">
           Elevate your site's visibility effortlessly with AI, where smart
           technology meets user-friendly SEO tools.
         </p>
+        {/* Desktop text */}
         <p className="hidden md:block text-xl text-white mb-6 max-w-2xl hero-text-animate">
           Elevate your site's visibility effortlessly with AI, where <br />
           smart technology meets user-friendly SEO tools.
         </p>
+        {/* Button */}
         <div className="hero-text-animate">
           <SecondaryButton text="Start for free" />
         </div>
       </div>
 
+      {/* Hero image */}
       <div className="hero-image hero-img-animate flex items-center justify-center rounded-[10px] overflow-hidden">
         <div className="hero-image-inner p-2 bg-white/10 border border-white/15">
           <Image
@@ -44,6 +50,7 @@ const Hero = () => {
           />
         </div>
       </div>
+      {/* Fade Gradient */}
       <div className="absolute bottom-0 w-full  h-[5rem] md:h-[20rem] bg-gradient-to-t from-black to-transparent" />
     </section>
   );
